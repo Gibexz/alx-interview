baf2fdd9ac349ea3bca9345022d64f109d4ae735
Gibexz <andrewetedu@gmail.com>
2023-10-01 23:45:07 +0100

    pascal_triangle implemetation

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,17 @@
+def pascal_triangle(n):
+    """ pascal_triangle """
+    if (n <= 0):
+        return []
+    
+    triangleP = []
+
+    for i in range(n):
+        row = [1]
+        if i > 0:
+            prev_row = triangleP[-1]
+            for j in range(i - 1):
+                row.append(prev_row[j] + prev_row[j + 1])
+            row.append(1)
+        triangleP.append(row)
+
+    return triangleP
