1ffd478e6e25a6c1e2895b4cb5050aaafc8e6503
Gibexz <andrewetedu@gmail.com>
2023-10-01 23:54:19 +0100

    pascal_triangle implemetation1

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,3 +1,11 @@
+#!/usr/bin/python3
+
+"""
+a function that returns a list of
+integers representing the Pascal's triangle of n
+"""
+
+
 def pascal_triangle(n):
     """ pascal_triangle """
     if (n <= 0):
