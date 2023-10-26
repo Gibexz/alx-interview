aaf0837d25830bf84a6f82274d7c263ef0eea32a
Gibexz <andrewetedu@gmail.com>
2023-10-26 22:17:36 +0100

    gpt coding test 12

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -9,19 +9,25 @@ def validUTF8(data):
     method that determines if a given data
     set represents a valid UTF-8 encoding.
     """
-    if len(data) == 1:
-        for i in data:
-            if i > 127:
+    num_bytes = 0  # Number of continuation bytes expected
+
+    for byte in data:
+        if num_bytes == 0:
+            # Check for multi-byte characters (2, 3, and 4 bytes)
+            if byte >> 5 == 0b110:
+                num_bytes = 1
+            elif byte >> 4 == 0b1110:
+                num_bytes = 2
+            elif byte >> 3 == 0b11110:
+                num_bytes = 3
+            # Check for invalid start bytes
+            elif byte >> 7 == 1:
                 return False
-            else:
-                return True
-    elif len(data) > 1:
-        if len(data) == 2 and data[0] > 223:
-            return False
 
         else:
-            new_data = data[1:]
-            for i in range(len(new_data)):
-                if new_data[i] > 191:
-                    return False
-    return True
+            # Check for continuation bytes
+            if byte >> 6 != 0b10:
+                return False  # Invalid continuation byte
+            num_bytes -= 1
+
+    return num_bytes == 0
