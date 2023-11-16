a4ac3da4362b8713290f8516c729f4f0ab4d27e1
Gibexz <andrewetedu@gmail.com>
2023-11-16 22:36:02 +0100

    0x07-rotate_2d_matrix dir and readme

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -16,12 +16,12 @@ def validUTF8(data):
             else:
                 return True
     elif len(data) > 1:
-        if len(data) == 2 and data[0] > 223:
+        if len(data) == 2 and data[0] > 223: #check for 2 byte
             return False
 
         else:
             new_data = data[1:]
             for i in range(len(new_data)):
-                if new_data[i] > 191:
+                if new_data[i] > 191: # contination value limit check. Maxed at 10111111 == 191
                     return False
     return True
